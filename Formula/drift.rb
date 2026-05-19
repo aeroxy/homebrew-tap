76882c003a55e7a220774abfc87bb17d56205dc5
Aero <aero.windwalker@gmail.com>
2026-05-20 01:39:00 +0800

    chore: update drift

--- Formula/drift.rb
+++ Formula/drift.rb
@@ -1,20 +1,20 @@
 class Drift < Formula
   desc "Encrypted bidirectional file transfer over WebSocket with an embedded web UI"
   homepage "https://github.com/aeroxy/drift"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/aeroxy/drift/releases/download/#{version}/drift_macos_arm64.zip"
-      sha256 "d1c5e52993e90bf0e1caf5b97939bcd482bee546701bac82f193d1b73e5065e4"
+      sha256 "def6517c0fc007f12eb21a43d3c1acf70d8bf6cf5774304dcd419813e46ca9a4"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/aeroxy/drift/releases/download/#{version}/drift_linux_x86_64.zip"
-      sha256 "9dee4249fb01c66e34c0cc67d33085370a22f76286acfa36135d41c35b68de95"
+      sha256 "f78e4aff97a94aa1a5da8d9f6ead457dcd9ef0deecc8046e3bb8115b399368f5"
     end
   end
 
