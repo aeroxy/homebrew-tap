42386615be455cca3bfe5105ac977153535aa695
Aero <aero.windwalker@gmail.com>
2026-08-08 15:32:41 +0800

    chore: add AstBro formula version 4.2.0 with new URL and checksum

--- ast-bro.rb
+++ ast-bro.rb
@@ -1,8 +1,8 @@
 class AstBro < Formula
   desc "Fast AST-based code navigation, search, rewrite, and log squeezing"
   homepage "https://github.com/aeroxy/ast-bro"
-  url "https://github.com/aeroxy/ast-bro/releases/download/4.0.0/ast-bro-macos-arm64.zip"
-  sha256 "19e0e5066d8b972028b4ab88812df4ccdcb64e0fcd8ad18f81410fd4720e308a"
+  url "https://github.com/aeroxy/ast-bro/releases/download/4.2.0/ast-bro-macos-arm64.zip"
+  sha256 "20a8d52b7db514250ab4164850ecae49a0f48ef8a0be6d1c1f1e9c32377f98ea"
   license "MIT"
 
   def install
